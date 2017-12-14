96eab3d1e2d1936b8e1dfb3dfc16a22935ed40aa
gorod <gorodenchuk.n@gmail.com>
2017-12-14 13:35:40 +0200

    second

--- src/test/java/Grid_2.java
+++ src/test/java/Grid_2.java
@@ -1,12 +1,17 @@
 
+import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,23 +23,43 @@ public class Grid_2 {
     @BeforeTest
     public void setUp() throws MalformedURLException {
 
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nazar_Gorodenchuk\\Desktop\\selenium\\src\\main\\resources\\resources\\chrome-win\\chromedriver.exe");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setBrowserName("chrome");
-        capabilities.setPlatform(Platform.WIN10);
-        webDriver = new RemoteWebDriver(new URL("http://10.25.14.48:5566"), capabilities);
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nazar_Gorodenchuk\\Desktop\\selenium\\src\\main\\resources\\resources\\chrome-win\\chromedriver.exe");
+//        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\gorod\\workspace\\patternsdemo\\src\\main\\java\\resources\\geckodriver.exe");
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+//        capabilities.setBrowserName("firefox");
+//        capabilities.setPlatform(Platform.WIN10);
+        WebDriver driver = new FirefoxDriver(capabilities);
+//        webDriver = new RemoteWebDriver(new URL("http://10.25.165.180:5566"), capabilities);
     }
 
-    @AfterTest
-    public void afterTest() {
+//    @AfterTest
+//    public void afterTest() {
+//        webDriver.quit();
+//    }
+
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\gorod\\workspace\\patternsdemo\\src\\main\\java\\resources\\geckodriver.exe");
+//        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//        capabilities.setCapability("marionette", true);
+//        WebDriver webDriver = new FirefoxDriver(capabilities);
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\gorod\\workspace\\gridtestversion\\src\\main\\resources\\resources\\chrome-win\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("Test-type");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("marionette", true);
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver webDriver = new ChromeDriver(capabilities);
+
+
+        Wait<WebDriver> wait = new WebDriverWait(webDriver, 3000);
+        webDriver.get("http://newtours.demoaut.com/");
+        Assert.assertEquals("Welcome: Mercury Tours", webDriver.getTitle());
         webDriver.quit();
     }
 
-    @Test
-    public void simpleTest() {
-        webDriver.get("http://newtours.demoaut.com/");
-        Assert.assertEquals("Welcome: Mercury Tours", webDriver.getTitle());
-    }
+
 
 }
